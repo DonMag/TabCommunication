d2e3f4b8e9ed63dd46494f546b21a2c25e1f779f
DonMag <DonMag@users.noreply.github.com>
2022-04-20 16:47:51 -0400

    Update ViewControllers.swift

--- TabCommunication/ViewControllers.swift
+++ TabCommunication/ViewControllers.swift
@@ -29,13 +29,64 @@ class HomeViewController: UIViewController {
 			resultVC.loadViewIfNeeded()
 			
 			// call the func, passing the button title
-			resultVC.doAThing(btnTitle)
+			//resultVC.doAThing(btnTitle)
 			
 			// switch to the 4th tab
 			tb.selectedIndex = 3
 		}
 		
 	}
+	
+	func searchBarSearchButtonClicked(_ searchQ: UISearchBar)
+	{
+		
+		print("1 - searchBarSearchButtonClicked")
+		
+		guard let btnTitle = searchQ.largeContentTitle else {
+			print("2 - FAILED to get searchQ.largeContentTitle")
+			return
+		}
+		
+		print("2 - got btnTitle", btnTitle)
+		
+		if let tb = self.tabBarController {
+			print("3 - got a tab bar controller reference")
+			if let controllers = tb.viewControllers {
+				print("4 - got controllers reference")
+				if controllers.count == 4 {
+					print("5 - we have 4 controllers")
+					if let resultVC = controllers[3] as? searchViewController {
+						
+						print("6 - got a reference to searchViewController")
+
+						// if we have not yet selected the 4th tab,
+						//    the view will not yet have been loaded
+						//    so make sure it is
+						resultVC.loadViewIfNeeded()
+						
+						// call the func, passing the button title
+						resultVC.doAthing(searchQ)
+						
+						// switch to the 4th tab
+						tb.selectedIndex = 3
+
+						print("7 - we should now be at searchViewController")
+						
+					} else {
+						print("6 - FAILED TO GET a reference to searchViewController")
+					}
+				} else {
+					print("5 - FAILED TO GET a reference to searchViewController")
+				}
+			} else {
+				print("4 - FAILED TO GET a reference to searchViewController")
+			}
+		} else {
+			print("3 - FAILED TO GET a reference to searchViewController")
+		}
+		
+	}
+
 	
 }
 class SecondViewController: UIViewController {
@@ -48,8 +99,11 @@ class SearchViewController: UIViewController {
 	
 	@IBOutlet var resultLabel: UILabel!
 	
-	func doAThing(_ str: String) {
-		resultLabel.text = str
+//	func doAThing(_ str: String) {
+//		resultLabel.text = str
+//	}
+	func doAthing(_ sb: UISearchBar) {
+		
 	}
 	
 }
